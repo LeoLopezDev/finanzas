cbf4500c02f5f19907f280997ad66d27611f1bc3
Leo López <lflorez@ikatechsolutions.com>
2019-07-16 15:51:55 -0500

    commit initial

---
+++ finanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finanzas
+//
+//  Created by Ikatech on 7/16/19.
+//  Copyright © 2019 leolopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
