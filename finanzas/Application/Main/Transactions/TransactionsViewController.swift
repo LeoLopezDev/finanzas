84882842cb45dd092939bdfce95da93581213a29
Leo López <lflorez@ikatechsolutions.com>
2019-07-22 17:12:50 -0500

    Se agrega tabla a vista de transacciones

---
+++ finanzas/Application/Main/Transactions/TransactionsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TransactionsViewController.swift
+//  finanzas
+//
+//  Created by Ikatech on 7/22/19.
+//  Copyright © 2019 leolopez. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TransactionsViewController: UITableViewDelegate{
+    
+}
+extension TransactionsViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+    
+    
+}
