72bd002d25d237c83d0d68519cfef80db17dd4f3
Leo López <lflorez@ikatechsolutions.com>
2019-08-09 16:57:01 -0500

    Se valida el email y password

--- finanzas/Application/Authentication/SignIn/SignInViewController.swift
+++ finanzas/Application/Authentication/SignIn/SignInViewController.swift
@@ -20,14 +20,22 @@ class SignInViewController: UIViewController {
     
     @IBAction func signIn(_ sender: Any) {
         
-        guard let email = self.emailTextField.text else {
+        guard let email = self.emailTextField.text, validate(text: email, regex: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}") else {
             return
         }
         
-        guard let password = self.passwordTextField.text else {
+        //Mínimo 8 caracteres al menos 1 alfabeto y 1 número
+        guard let password = self.passwordTextField.text, validate(text: password, regex: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$") else {
             return
         }
         
         print("Hola \(email), \(password)")
+    }
+    
+    func validate(text:String, regex:String) -> Bool {
+        let range = NSRange(location: 0, length: text.count)
+        let regex = try? NSRegularExpression(pattern: regex, options: [])
+        
+        return regex?.firstMatch(in: text, options: [], range: range) != nil
     }
 }
